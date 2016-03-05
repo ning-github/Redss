6cf0386a4c7cf8d26bc7969d4f3a4b04bcaf3943
Ning Xia <nxiamail@gmail.com>
2016-03-05 11:13:18 -0800

    Initial Commit

---
+++ src/reducers/reducer.books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Practical Node.js'},
+        {title: 'War and Peace'},
+        {title: 'Human Physiology'},
+        {title: 'Man Without Fear'}
+    ]
+}
