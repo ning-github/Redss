e723e1759f8814d03f0e0940b69783f6ddbe6ee9
Ning Xia <nxiamail@gmail.com>
2016-03-05 12:28:57 -0800

    Hook up ActiveBookReducer in rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer.books.js';
+import ActiveBookReducer from './reducer.activeBook';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
