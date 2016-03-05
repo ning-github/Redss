bd16179cc7b90814cf87c45e6058ff8e5305edaf
Ning Xia <nxiamail@gmail.com>
2016-03-05 15:29:32 -0800

    style SearchBar with bootstrap and higher priority custom stylesheet

--- src/containers/SearchBar.jsx
+++ src/containers/SearchBar.jsx
@@ -18,17 +18,19 @@ class SearchBar extends Component{
         this.setState({
             term: event.target.value
         });
+        console.log(this.state.term);
     }
 
     render() {
         return (
-            <form>
+            <form className="input-group">
                 <input
                     placeholder="Enter Search Here"
                     value={this.state.term}
-                    onChange={this.onInputChange}/>
-                <span>
-                    <button>Submit</button>
+                    onChange={this.onInputChange}
+                    className="form-control" />
+                <span className="input-group-btn">
+                    <button className="btn btn-primary">Submit</button>
                 </span>
 
             </form>
