fd63a4242a397d382162f22c9c71926da6bd0eed
Ning Xia <nxiamail@gmail.com>
2016-03-06 12:22:04 -0800

    Fix api key and import and endpoint prefix

--- src/const.js
+++ src/const.js
@@ -1,9 +1,9 @@
-import WEATHER_KEY from './keys.js';
+import {WEATHER_KEY} from './keys.js';
 
 export const actionTypeConst = {
     FETCH_WEATHER: "FETCH_WEATHER"
 }
 
 export const urlConst = {
-    WEATHER_URL: `api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_KEY}`;
+    WEATHER_URL: `http://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_KEY}`
 }
