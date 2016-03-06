ee5c4554a62985b20785e00ea6ff99c5bcf9d563
Ning Xia <nxiamail@gmail.com>
2016-03-06 11:30:49 -0800

    Return an http promise as the action created by fetchWeather action-creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
 import {actionTypeConst, urlConst} from '../const.js';
 
 export default function fetchWeather(city) {
     // will be making country code always United States
-    const finalUrl = `${urlConst.WEATHER_URL}&q=${city},us`;
+    let finalUrl = `${urlConst.WEATHER_URL}&q=${city},us`;
+    // this is an http promise
+    let apiRequest = axios.get(finalUrl);
+
     return {
         type: actionTypeConst.FETCH_WEATHER,
-        payload:
+        payload: apiRequest
     }
 }
