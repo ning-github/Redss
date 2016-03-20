1edb0bf4c0ba263178f0b6abbd6fe3bb66519a00
Ning Xia <nxiamail@gmail.com>
2016-03-20 11:19:37 -0700

    Change fallback state to an empty array

--- src/reducers/reducer.weather.js
+++ src/reducers/reducer.weather.js
@@ -1,6 +1,6 @@
 import {actionTypeConst} from '../const.js'
 
-export default function (state = null, action) {
+export default function (state = [], action) {
     switch (action.type) {
         case actionTypeConst.FETCH_WEATHER:
             return action.payload;
