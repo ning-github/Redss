42f93c63290a47b564069437a77a6c66288571e5
Ning Xia <nxiamail@gmail.com>
2016-04-03 19:49:47 -0700

    Return fetched city data as pure concatenated array

--- src/reducers/reducer.weather.js
+++ src/reducers/reducer.weather.js
@@ -3,7 +3,8 @@ import {actionTypeConst} from '../const.js'
 export default function (state = [], action) {
     switch (action.type) {
         case actionTypeConst.FETCH_WEATHER:
-            return action.payload;
+            return [action.payload, ...state];
+            // ^-- ES6 for state.concat([action.payload]);
             break;
         default:
             return state;
