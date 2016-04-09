acbb008151dcf73b39add63db6c0fcb4b0973f8c
Ning Xia <nxiamail@gmail.com>
2016-04-09 12:28:09 -0700

    Create table in WeatherList, import Weatherlist in App

--- src/containers/WeatherList.jsx
+++ src/containers/WeatherList.jsx
@@ -8,7 +8,15 @@ class WeatherList extends Component {
 
     render() {
         return (
-
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
         );
     }
 }
