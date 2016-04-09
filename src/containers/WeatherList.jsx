ba7fee48766e29193a422b67f88b7c67e7bba8ac
Ning Xia <nxiamail@gmail.com>
2016-04-09 12:24:03 -0700

    Create file for WeatherList

---
+++ src/containers/WeatherList.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default WeatherList;
