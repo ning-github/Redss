697db9d49f59ea68c0fd618bc53443d738a06986
Ning Xia <nxiamail@gmail.com>
2016-04-16 11:55:28 -0700

    Navigate programatically on Submit, navigate using Link when clicking Cancel button, style Cancel button with bootstrap classes

--- src/components/PostsNew.jsx
+++ src/components/PostsNew.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 // reduxForm is a special connect function that binds redux state to this component
 import { reduxForm } from 'redux-form';
 // actionCreator to pass to handleSubmit of reduxForm
-import {createNewPost} from '../actions/index';
+import { createNewPost } from '../actions/index';
 
 /* reduxForm is a Higher Order Component
     - just like connect
@@ -12,8 +13,23 @@ import {createNewPost} from '../actions/index';
 */
 
 class PostsNew extends Component {
+    static contextTypes = {
+        router: React.PropTypes.object.isRequired
+    };
+
     constructor(props) {
         super(props);
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    // extract out submisssion to allow routing too
+    onSubmit(props) {
+        // call actionCreator (which returns a Promise with thenable)
+        this.props.createNewPost(props)
+            .then(() => {
+                this.context.router.push('/');
+            });
     }
 
     render() {
@@ -24,27 +40,38 @@ class PostsNew extends Component {
             let handleSubmit = this.props.handleSubmit;
         */
         return (
-            <form onSubmit={handleSubmit(this.props.createNewPost)}>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
                 {/* ^-- actionCreator mapped into props by reduxForm connector */}
-                <h3>Create a New Post</h3>
+                <h3 style={{marginTop: '40px'}}>Create a New Post</h3>
 
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title}/>
-                    {title.touched ? title.error : null}
+                    <div className="text-help">
+                        {title.touched ? title.error : null}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories}/>
+                    <div className="text-help">
+                        {categories.touched ? categories.error : null}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea type="text" className="form-control" {...content}/>
+                    <div className="text-help">
+                        {content.touched ? content.error : null}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
+
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+
             </form>
         )
     }
@@ -58,6 +85,14 @@ function validate(values) {
     // title
     if (!values.title) {
         errors.title = 'Enter a title';
+    }
+    // categories
+    if (!values.categories) {
+        errors.categories = 'Enter a category';
+    }
+    // title
+    if (!values.content) {
+        errors.content = 'Enter content';
     }
 
     return errors;
